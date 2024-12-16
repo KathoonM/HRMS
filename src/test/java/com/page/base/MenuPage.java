34b7df4ac2e903e67280a1e8eced919c045cbace
KathoonM <kathoon.j@tickingminds.com>
2024-12-16 15:36:57 +0530

    employeemodule

---
+++ src/test/java/com/page/base/MenuPage.java
@@ -0,0 +1,79 @@
+package com.page.base;
+
+import com.data.dynamic.FakerDataFactory;
+import com.github.javafaker.Faker;
+
+import static com.data.dynamic.FakerDataFactory.faker;
+
+public class MenuPage extends CognitestPageHook {
+    public FakerDataFactory dataFactory ;
+    // Locator for username field
+    protected String usernameField = "//input[@name = 'username']";
+
+    // Locator for password field
+    protected String passwordField = "//input[@name = 'password']";
+
+    // Locator for login button
+    protected String loginButton = "//button[@type= 'submit']";
+
+    protected String firstName = "//input[@name = 'firstName']";
+
+    protected String middleName = "//input[@name = 'middleName']";
+
+    protected String lastName = "//input[@name = 'lastName']";
+
+
+    public MenuPage navigateToLoginPage() {
+        getPage().navigate("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        return this;
+    }
+
+    public MenuPage enterUsername(String username) {
+        dataFactory = new FakerDataFactory();
+        typeWithType("//input[@name = 'username']", "Admin", "Username");
+        return this;
+    }
+
+    public MenuPage enterPassword(String password) {
+        typeWithType("//input[@name = 'password']", "admin123", "Password");
+        return this;
+    }
+
+    public MenuPage clickOnLoginButton() {
+        click("//button[@type= 'submit']", "Login");
+        return this;
+    }
+    public MenuPage navigateToPIMModule() {
+        // Assuming the locator for PIM module navigation
+        String pimModuleLocator = "//span[.='PIM']";
+        click(pimModuleLocator, "PIM Module");
+        return this;
+    }
+
+    public MenuPage clickOnAddEmployeeButton() {
+        String addEmployeeButtonLocator = "//a[.='Add Employee']";
+        click(addEmployeeButtonLocator, "Add Employee Button");
+        return this;
+    }
+
+    public MenuPage enterFirstName() {
+        type("//input[@name = 'firstName']", FakerDataFactory.getFirstName(), "First Name");
+        return this;
+    }
+    public MenuPage enterMiddleName() {
+        type("//input[@name = 'middleName']", FakerDataFactory.getMiddleName(), "Middle Name");
+        return this;
+    }
+    public MenuPage enterLastName() {
+        type("//input[@name = 'lastName']", FakerDataFactory.getLastName(), "Last Name");
+        return this;
+    }
+
+    public MenuPage clickOnSaveButton() {
+        String saveButtonLocator = "//button[@type ='submit']";
+        click(saveButtonLocator, "Save Button");
+        return this;
+
+
+    }
+}
